16d8435634c4df8c9c736ac21dc7179ed9017c7e
archie0732 <121162902+archie0732@users.noreply.github.com>
2025-02-02 15:34:35 +0800

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,43 @@
+import { writeFileSync } from "fs";
+import { resolve } from "path";
+import { connect } from "puppeteer-real-browser";
+
+async function test() {
+  const { browser, page } = await connect({
+    headless: false,
+
+    args: [],
+
+    customConfig: {},
+
+    turnstile: true,
+
+    connectOption: {},
+
+    disableXvfb: false,
+    ignoreAllFlags: false,
+    // proxy:{
+    //     host:'<proxy-host>',
+    //     port:'<proxy-port>',
+    //     username:'<proxy-username>',
+    //     password:'<proxy-password>'
+    // }
+  });
+  await page.goto("https://nhentai.net/api/search?query=*", { waitUntil: "networkidle2" });
+
+  await new Promise((resolve) => setTimeout(resolve, 5_000));
+
+  const cookies = await page.cookies();
+  const cfClearance = cookies.find(cookie => cookie.name === "cf_clearance")
+
+
+  if(cfClearance){
+    writeFileSync(resolve('data','cf.txt'),`cf_clearance=${cfClearance.value}`,'utf-8')
+  }
+  else{
+    console.log('somthing wrong')
+  }
+  page.close()
+}
+
+test();
