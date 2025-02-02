b997dae1c8f5530d3c557d4583f64fa6b1c54e13
archie0732 <121162902+archie0732@users.noreply.github.com>
2025-02-02 17:54:39 +0800

    Update get-cf-clearance.js

--- src/get-cf-clearance.js
+++ src/get-cf-clearance.js
@@ -5,40 +5,40 @@ import { connect } from "puppeteer-real-browser";
 async function test() {
   const { browser, page } = await connect({
     headless: false,
-
     args: [],
-
     customConfig: {},
-
     turnstile: true,
-
     connectOption: {},
-
     disableXvfb: false,
     ignoreAllFlags: false,
-    // proxy:{
-    //     host:'<proxy-host>',
-    //     port:'<proxy-port>',
-    //     username:'<proxy-username>',
-    //     password:'<proxy-password>'
-    // }
   });
+
   await page.goto("https://nhentai.net/api/search?query=*", { waitUntil: "networkidle2" });
 
   await new Promise((resolve) => setTimeout(resolve, 15_000));
 
   const cookies = await page.cookies();
-  const cfClearance = cookies.find(cookie => cookie.name === "cf_clearance")
+  const cfClearance = cookies.find((cookie) => cookie.name === "cf_clearance");
 
+  // 獲取 Puppeteer 的 user-agent
+  const userAgent = await page.browser().userAgent();
 
-  if(cfClearance){
-    console.log('sucess get token: ',cfClearance.value);
-    writeFileSync(resolve('data','cf.txt'),`cf_clearance=${cfClearance.value}`,'utf-8')
+  if (cfClearance) {
+    console.log("success get token:", cfClearance.value);
+
+    // 準備 JSON 資料
+    const jsonData = {
+      cf_clearance: cfClearance.value,
+      user_agent: userAgent,
+    };
+
+    // 將資料寫入 cf.json
+    writeFileSync(resolve("data", "cf.json"), JSON.stringify(jsonData, null, 2), "utf-8");
+  } else {
+    console.log("something wrong");
   }
-  else{
-    console.log('somthing wrong')
-  }
-  page.close()
+
+  await page.close();
 }
 
 test();
